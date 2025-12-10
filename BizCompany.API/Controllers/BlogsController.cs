5589c7cf28c635c5618cac52d2ce9c9cc3bab14a
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-12-10 18:30:50 +0300

    Add BlogsController and BlogRepository for blog API
    Introduced BlogsController with a GET endpoint to retrieve all blogs using dependency injection. Added BlogRepository with method to fetch blogs and related entities, filtering out deleted records and optimizing queries with EF Core.

---
+++ BizCompany.API/Controllers/BlogsController.cs
@@ -0,0 +1,25 @@
+﻿using BizCompany.API.DataAccess;
+using BizCompany.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BizCompany.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController(IBaseRepository<Blog> repository)
+        : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await repository.GetAllAsync();
+            if (values == null || !values.Any())
+            {
+                return NotFound("No blogs found.");
+            }
+            return Ok(values);
+        }
+
+    }
+}
---
+++ BizCompany.API/DataAccess/BlogRepository.cs
@@ -0,0 +1,28 @@
+﻿using BizCompany.API.Context;
+using BizCompany.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BizCompany.API.DataAccess
+{
+    public class BlogRepository(AppDbContext context)
+    {
+        public async Task<List<Blog>> GetBlogsWithDetailsAsync()
+        {
+            var values = await context.Blogs
+                .Where(b => !b.IsDeleted && b.Writer != null && !b.Writer.IsDeleted
+                            && b.Category != null && !b.Category.IsDeleted
+                            && b.BlogTags != null && b.BlogTags.All(bt => !bt.IsDeleted)
+                            && b.Comments != null && b.Comments.All(c => !c.IsDeleted))
+                .Include(w => w.Writer)
+                .Include(bc => bc.Category)
+                .Include(bt => bt.BlogTags!)
+                    .ThenInclude(t => t.Tag)
+                .Include(c => c.Comments!)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return values;
+        }
+    }
+}
